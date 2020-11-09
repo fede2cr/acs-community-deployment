58dd2081c7be9cdda631f6f326c92a26f5a2db30
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-11-09 17:35:21 -0600

    Agregando fixperms.sh

---
+++ fixperms.sh
@@ -0,0 +1,3 @@
+mkdir -p volumes/logs/alfresco volumes/data/alf-repo-data volumes/logs/postgres/ volumes/data/postgres-data
+sudo chown 33000:33000 volumes/logs/alfresco volumes/data/alf-repo-data
+sudo chown 999:999 volumes/logs/postgres/ volumes/data/postgres-data

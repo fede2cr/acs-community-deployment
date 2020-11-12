57853e8efa7b41dc480ab441135058be7bc348a7
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-11-12 06:05:44 -0600

    Creamos directorio para certs en fixperms

--- fixperms.sh
+++ fixperms.sh
@@ -1,4 +1,4 @@
-mkdir -p volumes/logs/alfresco volumes/data/alf-repo-data volumes/logs/postgres/ volumes/data/postgres-data volumes/data/solr-data
+mkdir -p volumes/logs/alfresco volumes/data/alf-repo-data volumes/logs/postgres/ volumes/data/postgres-data volumes/data/solr-data volumes/data/certs/conf
 chown 33000:33000 volumes/logs/alfresco volumes/data/alf-repo-data
 chown 999:999 volumes/logs/postgres/ volumes/data/postgres-data
 chown 33007:33007 volumes/data/solr-data

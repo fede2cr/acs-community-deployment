15aca2f15b60e4f3b22ab33c4fff107f5cd56a68
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-11-17 17:47:01 -0600

    No interactive docker-compose for install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ git clone https://github.com/Greencorecr/acs-community-deployment.git
 cd acs-community-deployment/docker-compose
 sudo ../fixperms.sh
 sudo openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=newdomain.com" -addext "subjectAltName=DNS:newdomain.com" -newkey rsa:2048 -out volumes/data/certs/conf/fullchain.pem -keyout volumes/data/certs/conf/privkey.pem
-#docker-compose --no-color up #-d
+COMPOSE_INTERACTIVE_NO_CLI=1 docker-compose -d up 

e586decee5ddc52e033e38d39e8913dd6e779b58
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-11-18 08:36:38 -0600

    Reemplazando pruebas locales con wget por python

--- test/alf_test.py
+++ test/alf_test.py
@@ -3,6 +3,26 @@ import requests
 
 class ShareTest(unittest.TestCase):
 
+    def test_share_avl(self):
+        r = requests.get('http://localhost/share/')
+        r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_alf_avl(self):
+        r = requests.get('http://localhost/alfresco/')
+        r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_share_redir_avl(self):
+        r = requests.get('http://localhost/nonexistent')
+        r.status_code
+        self.assertEqual(r.status_code, 200)
+
+    def test_fail_avl(self):
+        r = requests.get('http://localhost/share/nonexistent')
+        r.status_code
+        self.assertEqual(r.status_code, 404)
+
     def test_share_login(self):
         r = requests.get('http://localhost/alfresco/service/index', auth=('admin', 'admin'))
         r.status_code

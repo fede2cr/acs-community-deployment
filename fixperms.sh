d11835ce736d971f550f63edba2967c780f4dfef
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-11-26 10:31:49 -0600

    Eliminado dependencia de fixperms

--- fixperms.sh
+++ fixperms.sh
@@ -1,4 +1,5 @@
-mkdir -p volumes/logs/alfresco volumes/data/alf-repo-data volumes/logs/postgres/ volumes/data/postgres-data volumes/data/solr-data volumes/data/certs/conf
-chown 33000:33000 volumes/logs/alfresco volumes/data/alf-repo-data
+#mkdir -p volumes/logs/alfresco volumes/data/alf-repo-data volumes/logs/postgres/ volumes/data/postgres-data volumes/data/solr-data volumes/data/certs/conf
+mkdir -p volumes/logs/postgres/ volumes/data/postgres-data volumes/data/solr-data volumes/data/certs/conf
+#chown 33000:33000 volumes/logs/alfresco volumes/data/alf-repo-data
 chown 999:999 volumes/logs/postgres/ volumes/data/postgres-data
 chown 33007:33007 volumes/data/solr-data

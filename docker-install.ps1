daa22ec1ac1bcb5a12846c9debd9aba664b7f80f
Alvaro Figueroa <alvaro@greencore.co.cr>
2020-12-07 06:34:38 -0600

    Agregando scripts en powershell para instalar sobre windows 2019+hyperv

--- docker-install.ps1
+++ docker-install.ps1
@@ -1,19 +1,20 @@
 Install-WindowsFeature -Name Hyper-V,Containers -IncludeAllSubFeature -IncludeManagementTools
-Import-Module -Name DockerMSFTProvider -Force
-Import-Packageprovider -Name DockerMSFTProvider -Force
-[Environment]::SetEnvironmentVariable(“LCOW_SUPPORTED”, “1”, “Machine”)
-$configfile = @”
+Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+Install-Package -Name docker -ProviderName DockerMsftProvider
+[Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
+$configfile = @"
 
 {
 
-    “experimental”: true
+    "experimental": true
 
 }
 
-“@
+"@
 
+New-Item -itemtype directory -path C:\ProgramData\docker\config
 $configfile|Out-File -FilePath C:\ProgramData\docker\config\daemon.json -Encoding ascii -Force
 Invoke-WebRequest -Uri "https://github.com/linuxkit/lcow/releases/download/v4.14.35-v0.3.9/release.zip" -UseBasicParsing -OutFile release.zip
-Expand-Archive release.zip -DestinationPath “$Env:ProgramFiles\Linux Containers\.”
-[Environment]::SetEnvironmentVariable(“LCOW_API_PLATFORM_IF_OMITTED”, “linux”, “Machine”)
-Restart-Computer
+Expand-Archive release.zip -DestinationPath "$Env:ProgramFiles\Linux Containers\."
+[Environment]::SetEnvironmentVariable("LCOW_API_PLATFORM_IF_OMITTED", "linux", "Machine")
+#Restart-Computer
